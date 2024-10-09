e4acb77ed6d7e621af09239c2c58955fcc2182fb
HemisCoding <plushytowel@gmail.com>
2024-10-09 16:20:58 +0300

    for cosmin

--- src/components/DocImporter.vue
+++ src/components/DocImporter.vue
@@ -6,12 +6,47 @@
           <p>This is where documents-related content will go.</p>
         </v-col>
       </v-row>
+      <v-row>
+        <v-btn @click="openGoogleDrive">
+            <v-icon left>mdi-upload</v-icon>
+            Upload to Google Drive
+        </v-btn>
+      </v-row>
     </v-container>
   </template>
+
+<script>
+import { gapi } from 'gapi-script';
+
+
   
-  <script>
-  export default {
-    name: 'DocImporter',
+export default {
+    data() {
+      return {
+        clientId: 'YOUR_CLIENT_ID.apps.googleusercontent.com', // Obținut de la Google Cloud
+        apiKey: 'YOUR_API_KEY', // Obținut de la Google Cloud
+        scope: 'https://www.googleapis.com/auth/drive.file',
+        discoveryDocs: ['https://www.googleapis.com/discovery/v1/apis/drive/v3/rest'],
+      };
+    },
+    
+    methods: {
+      openGoogleDrive() {
+        gapi.load('client:auth2', () => {
+          gapi.client.init({
+            apiKey: this.apiKey,
+            clientId: this.clientId,
+            discoveryDocs: this.discoveryDocs,
+            scope: this.scope,
+          }).then(() => {
+            gapi.auth2.getAuthInstance().signIn().then(() => {
+              // Redirectează către Google Drive sau folosește API-ul pentru a deschide un picker
+              window.open('https://drive.google.com/drive/my-drive', '_blank');
+            });
+          });
+        });
+      },
+    },
   };
   </script>
   
